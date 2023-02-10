6ab9bcb457088f1cc5897881ecba37b19ec5f1a7
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-02-10 11:43:20 +0100

    home pagina

--- index.php
+++ index.php
@@ -1,22 +1,55 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/main.css">
-  <title>My Newspaper</title>
-</head>
-<body>
-  <header>
-    <h1>My Newspaper</h1>
-  </header>
-  <nav>
-    <ul>
-      <li><a href="#section1">Section 1</a></li>
-      <li><a href="#section2">Section 2</a></li>
-      <li><a href="#section3">Section 3</a></li>
-    </ul>
-  </nav>
-  <main>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD - Nederlands Dagblad</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1 class="logo">AD - Nederlands Dagblad</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Nieuws</a></li>
+            <li><a href="#">Sport</a></li>
+            <li><a href="#">Entertainment</a></li>
+            <li><a href="#">Regio</a></li>
+            <li><a href="#">Opinie</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <section class="featured">
+          <h2>Uitgelicht</h2>
+          <div class="featured-articles">
+            <article>
+              <h3><a href="#">Article 1</a></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in blandit lorem.</p>
+            </article>
+            <article>
+              <h3><a href="#">Article 2</a></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in blandit lorem.</p>
+            </article>
+            <article>
+              <h3><a href="#">Article 3</a></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in blandit lorem.</p>
+            </article>
+          </div>
+        </section>
+        <aside>
+          <h2>Advertenties</h2>
+          <div class="ads">
+            <img src="ad1.jpg" alt="Advertentie 1">
+            <img src="ad2.jpg" alt="Advertentie 2">
+            <img src="ad3.jpg" alt="Advertentie 3">
+          </div>
+        </aside>
+      </div>
+
     <section id="section1">
       <h2>Section 1</h2>
       <p> TEST Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, risus a tincidunt laoreet, magna lorem pellentesque magna, eu laoreet lacus sem vel velit.</p>
