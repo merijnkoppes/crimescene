c5ab1311169c36594920bdffafa58a380a932dae
luuk353 <1198506@student.roc-nijmegen.nl>
2023-02-14 09:55:33 +0100

    login fixed and start aan opmaak

--- includes/nav.php
+++ includes/nav.php
@@ -1,10 +1,11 @@
 <header>
 <img id="logo" src = "img/crimescenelogo.jpg" alt = "werkt niet man">
-    <h1>My Newspaper</h1>
+    <h1>nieuwsbrief </h1>
   </header>
   
 <nav>
     <ul>
+      <li><a href="index.php">HOME</a> </li>
       <li><a href="#section1">Section 1</a></li>
       <li><a href="#section2">Section 2</a></li>
       <li><a href="#section3">Section 3</a></li>
