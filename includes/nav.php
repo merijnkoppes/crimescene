3b47ca98c7e87e158216e6bd7ebcbe3e0fa85c22
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2023-02-15 14:21:07 +0100

    nav aanpassingen

--- includes/nav.php
+++ includes/nav.php
@@ -1,14 +1,15 @@
 <header>
-<img id="logo" src = "img/crimescenelogo.jpg" alt = "werkt niet man">
-    <h1>EscapeRoom</h1>
+
+<a href="index.php"><img id="logo" src = "img/crimescenelogo.jpg" alt = "werkt niet man"></a>
+<h1>EscapeRoom</h1>
   </header>
   
 <nav>
     <ul>
-      <li><a href="index.php">HOME</a> </li>
-      <li><a href="artikel1.php">Section 1</a></li>
-      <li><a href="artikel2.php">Section 2</a></li>
-      <li><a href="artikel3.php">Section 3</a></li>
+
+      <li><a href="artikel1.php">artikel1</a></li>
+      <li><a href="artikel2.php">artikel2</a></li>
+      <li><a href="artikel3.php">artikel3</a></li>
       <li><a href="login.php">Login</a></li>
     </ul>
 </nav>
