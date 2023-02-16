81a9321504b4257dc33ea9b8d2fc1dad15161adb
Aiden Raaphorst <aidenraaphorst@gmail.com>
2023-02-16 09:46:31 +0100

    remove db connection due to static information

--- includes/loginvalidate.php
+++ includes/loginvalidate.php
@@ -1,28 +1,13 @@
 <?php
     session_start();
-    include("connect.php");
-    // header("Location: ../index.php");
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $sql = "SELECT * FROM users WHERE username = :username";
-        $stmt = $connect -> prepare($sql);
-        $stmt -> bindParam(":username", $_POST["username"]);
-        $stmt -> execute();
-        $result = $stmt -> fetchAll();
-        
-        foreach($result as $res)  { 
-            if($_POST["password"] == $res["password"]) {
-                header("Location:../ingelogd.php");
-                return;
-            }
+        if($_POST['username'] == "billy" && $_POST['password'] == "janssen") {
+            header("Location:../ingelogd.php");
+            return;
         }
-        header("Location:../foutantwoord.php");
-    }
-    // if (isset($_SESSION["username"]) == TRUE){
 
-    // if(isset($_POST["password"]) == TRUE)
-    // { 
-    //     header("Location: ../ingelogd.php");
-    // }
-    // }
-        ?>
+        header("Location:../foutantwoord.php");
+        return;
+    }
+?>
